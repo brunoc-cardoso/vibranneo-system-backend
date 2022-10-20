21862ada2ff5812024b3d89840d923473f38491c
brunoc-cardoso <brunocpessoal@gmail.com>
2022-10-20 14:58:27 -0300

    project configured for deploy

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/healthy', (request: Request, response: Response) => {
   response.status(200).json({
     message: 'Server is running',
   });
 });
+
+// app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
